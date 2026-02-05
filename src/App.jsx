f69485d0c3f65e139973d0ee41fa0eae071f3790
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2026-02-05 13:36:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+  const handelClick =()=>{
+    alert("I Love You")
+  }
+  const handelClickTwo = (sum)=>{
+    const result = sum +5;
+    alert(result);
+  }
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <button onClick={handelClick}>Click Me</button>
+      <button onClick={()=>handelClickTwo (10)}>Click Me</button>
+    </>
+  )
+}
+
+export default App
+    
