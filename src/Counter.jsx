db81f6b83b947d92b4d7b9a735bc80204a307f7a
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2026-02-08 13:13:20 +0600

    export file

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,9 @@
-function Counter (){
-    const counterStye
+export default function Counter (){
+    const counterStye = {
+        border: '2px solid green'
+    }
     return(
-        <div>
+        <div style={counterStye}>
             <h3>Count: </h3>
         </div>
     )
