39b817f660ed8bb10f657072414da0ca3f0e736d
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2026-04-08 19:55:51 +0600

    added the new line

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 
 import './App.css'
-import 
+ 
 
 function App() {
-  const handelClick =()=>{
-    alert("I Love You")
-  }
-  const handelClickTwo = (sum)=>{
-    const result = sum +7;
-    alert(result);
-  }
-
+  
+function handelClick(){
+alert('click me')
+}
+const handelClick2=()=>{
+  alert('React Core concept Part Two')
+}
+const handelClick3 =(sum)=>{
+  const result = sum + 5;
+  alert(result)
+}
   return (
     <>
       <h1>Vite + React</h1>
       <button onClick={handelClick}>Click Me</button>
-      <button onClick={()=>handelClickTwo (10)}>Click Me</button>
+      <button onClick={handelClick2}>Click Me 2</button>
+      <button onClick={()=>handelClick3(5)}>Click Me 3</button>
     </>
   )
 }
